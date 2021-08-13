2e17c894c4850819716a30c98c3e6e41f8073516
jamesgilbs <73652769+jamesgilbs@users.noreply.github.com>
2021-08-13 09:49:10 -0300

    Project started

---
+++ iot-mqtt-atmosfera/printValueBME280.h
@@ -0,0 +1,32 @@
+//BME280 I2C
+#define SEALEVELPRESSURE_HPA (1013.25)
+TwoWire I2CBME = TwoWire(0);
+Adafruit_BME280 bme;
+//unsigned long delayTime;
+
+/************ BME280 **************/
+void printValuesBME280() {
+  
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
+  
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
+}
